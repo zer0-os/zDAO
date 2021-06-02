4bc97273c48623b97ea0541048e0a73ab1eab9d5
dreamienb <dreamienb@gmail.com>
2021-06-02 16:03:06 -0600

    test cases setup

--- test/zdao_test.js
+++ test/zdao_test.js
@@ -1,14 +1,4 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
-const { toEthSignedMessageHash, fixSignature } = require('./helpers/sign');
-
-const { expect } = require('chai');
-
-const zAuction = artifacts.require('zAuction');
-const nftcontract = artifacts.require('ERC721TestToken');
-const erc20 = artifacts.require('ERC20TestToken')
-
-const TEST_MESSAGE = web3.utils.sha3('OpenZeppelin');
-const WRONG_MESSAGE = web3.utils.sha3('Nope');
+const ZDao = artifacts.require('ZDao');
 
 contract('ZDao', function (accounts) {
   const [ other ] = accounts;
@@ -19,9 +9,86 @@ contract('ZDao', function (accounts) {
   beforeEach(async function () {
 
   });
+  //inactive
+  context('with valid ZNA', function () {
+    it('should successfully create an associated ZDAO', async function () {
 
-  context('', function () {
-    it('', async function () {
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully set DAO token', async function () {
+
+    });
+  });
+  
+  context('with inactive ZDAO', function () {
+    it('should successfully set Voting type absolute', async function () {
+
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully set Voting type relative', async function () {
+
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully set proposal time limit', async function () {
+
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully set proposal success threshold', async function () {
+
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully add contract address to safelist', async function () {
+
+    });
+  });
+  context('with inactive ZDAO', function () {
+    it('should successfully activate DAO', async function () {
+
+    });
+  });
+  //active
+  context('with active ZDAO', function () {
+    it('should successfully propose adding contract address to safelist', async function () {
+
+    });
+  });
+  context('on reaching successful vote threshold', function () {
+    it('should successfully add contract address to safelist', async function () {
+
+    });
+  });
+  context('with safelisted contract', function () {
+    it('should successfully propose contract call', async function () {
+
+    });
+  });
+  context('on reaching successful vote threshold', function () {
+    it('should successfully execute contract call', async function () {
+
+    });
+  });
+  context('with active ZDAO', function () {
+    it('should successfully propose adding new neuron', async function () {
+
+    });
+  });
+  context('on reaching successful vote treshold', function () {
+    it('should successfully add neuron', async function () {
+
+    });
+  });
+  context('with neuron installed', function () {
+    it('should successfully execute call from neuron', async function () {
+
+    });
+  });
+  context('with active ZDAO', function () {
+    it('should successfully upgrade DAO and transfer state', async function () {
 
     });
   });
