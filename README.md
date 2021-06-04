8c1ba1dc781b149ed6289b73287e114c1b8f2fe7
dreamienb <dreamienb@gmail.com>
2021-06-04 10:07:59 -0600

    add diagram

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ npm install
 ```bash
 truffle test
 ```
+
+## Spec
+
+https://drive.google.com/file/d/188xO9H1V0Gx2UoFPskYSEBDgFPoi-MPk/view?usp=sharing
--- test/zdao_test.js
+++ test/zdao_test.js
@@ -9,7 +9,6 @@ contract('ZDao', function (accounts) {
   beforeEach(async function () {
 
   });
-  //inactive
   context('with valid ZNA', function () {
     it('should successfully create an associated ZDAO', async function () {
 
@@ -20,39 +19,34 @@ contract('ZDao', function (accounts) {
 
     });
   });
-  
+  //absolute
   context('with inactive ZDAO', function () {
     it('should successfully set Voting type absolute', async function () {
 
     });
   });
-  context('with inactive ZDAO', function () {
-    it('should successfully set Voting type relative', async function () {
-
-    });
-  });
-  context('with inactive ZDAO', function () {
+  context('with inactive absolute ZDAO', function () {
     it('should successfully set proposal time limit', async function () {
 
     });
   });
-  context('with inactive ZDAO', function () {
+  context('with inactive absolute ZDAO', function () {
     it('should successfully set proposal success threshold', async function () {
 
     });
   });
-  context('with inactive ZDAO', function () {
+  context('with inactive absolute ZDAO', function () {
     it('should successfully add contract address to safelist', async function () {
 
     });
   });
-  context('with inactive ZDAO', function () {
+  context('with inactive absolute ZDAO', function () {
     it('should successfully activate DAO', async function () {
 
     });
   });
-  //active
-  context('with active ZDAO', function () {
+
+  context('with active absolute ZDAO', function () {
     it('should successfully propose adding contract address to safelist', async function () {
 
     });
@@ -72,7 +66,7 @@ contract('ZDao', function (accounts) {
 
     });
   });
-  context('with active ZDAO', function () {
+  context('with active absolute ZDAO', function () {
     it('should successfully propose adding new neuron', async function () {
 
     });
@@ -87,7 +81,74 @@ contract('ZDao', function (accounts) {
 
     });
   });
-  context('with active ZDAO', function () {
+  context('with active absolute ZDAO', function () {
+    it('should successfully upgrade DAO and transfer state', async function () {
+
+    });
+  });
+  //relative
+  context('with inactive relative ZDAO', function () {
+    it('should successfully set Voting type relative', async function () {
+
+    });
+  });
+  context('with inactive relative ZDAO', function () {
+    it('should successfully set proposal time limit', async function () {
+
+    });
+  });
+  context('with inactive relative ZDAO', function () {
+    it('should successfully set proposal success threshold', async function () {
+
+    });
+  });
+  context('with inactive relative ZDAO', function () {
+    it('should successfully add contract address to safelist', async function () {
+
+    });
+  });
+  context('with inactive relative ZDAO', function () {
+    it('should successfully activate DAO', async function () {
+
+    });
+  });
+
+  context('with active relative ZDAO', function () {
+    it('should successfully propose adding contract address to safelist', async function () {
+
+    });
+  });
+  context('on reaching successful vote threshold', function () {
+    it('should successfully add contract address to safelist', async function () {
+
+    });
+  });
+  context('with safelisted contract', function () {
+    it('should successfully propose contract call', async function () {
+
+    });
+  });
+  context('on reaching successful vote threshold', function () {
+    it('should successfully execute contract call', async function () {
+
+    });
+  });
+  context('with active relative ZDAO', function () {
+    it('should successfully propose adding new neuron', async function () {
+
+    });
+  });
+  context('on reaching successful vote treshold', function () {
+    it('should successfully add neuron', async function () {
+
+    });
+  });
+  context('with neuron installed', function () {
+    it('should successfully execute call from neuron', async function () {
+
+    });
+  });
+  context('with active relative ZDAO', function () {
     it('should successfully upgrade DAO and transfer state', async function () {
 
     });
