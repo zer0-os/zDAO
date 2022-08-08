8c1e66da8cab77ecd0f8139f963f30a6a1c8776b
deep-quality-dev <hungrywarrior081@gmail.com>
2022-08-08 11:58:58 -0400

    refactor: split mapping files

--- subgraph/src/mapping.registry.ts
+++ subgraph/src/mapping.registry.ts
@@ -114,22 +114,3 @@ export function handleLinkRemoved(event: LinkRemoved): void {
 
   store.remove('ZNAAssociation', zNAId);
 }
-
-export function handleProposalExecuted(event: ProposalExecuted): void {
-  const platformType = event.params._platformType.toI32();
-  const proposalHash = event.params._proposalHash;
-  const token = event.params._token;
-  const recipient = event.params._to;
-  const amount = event.params._amount;
-
-  log.info("handleProposalExecuted, called {}, {}", [platformType.toString(), proposalHash.toHexString()]);
-
-  const id = `${platformType}-${proposalHash}`;
-  const proposal: ExecutedProposal = new ExecutedProposal(id);
-  proposal.platformType = platformType;
-  proposal.proposalHash = proposalHash;
-  proposal.token = token;
-  proposal.recipient = recipient;
-  proposal.amount = amount;
-  proposal.save();
-}
