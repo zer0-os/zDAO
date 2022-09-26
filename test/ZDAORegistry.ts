8cc0e2c0b99406ef1ec1ef32cbde707495aa159b
deep-quality-dev <hungrywarrior081@gmail.com>
2022-09-26 16:46:41 -0400

    fix: remove zNA association in test scripts

--- test/ZDAORegistry.ts
+++ test/ZDAORegistry.ts
@@ -11,13 +11,12 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import {
   IERC20Upgradeable,
   IEthereumStateSender,
-  IZNSHub,
   EthereumZDAOChef,
   EthereumZDAOChef__factory,
   SnapshotZDAOChef,
   SnapshotZDAOChef__factory,
   ZDAORegistry,
-  ZDAORegistry__factory
+  ZDAORegistry__factory,
 } from "../types";
 import { PlatformType } from "../scripts/shared/config";
 import { BigNumber } from "ethers";
@@ -25,8 +24,7 @@ import { ZDAOConfig } from "./shared/types";
 
 chai.use(smock.matchers);
 
-interface zNAPair {
-  zNA: number;
+interface zDAOPair {
   gnosisSafe: string;
   name: string;
   ens: string;
@@ -38,19 +36,17 @@ describe("ZDAORegistry", function () {
     user2: SignerWithAddress,
     user3: SignerWithAddress;
 
-  let znsHub: FakeContract<IZNSHub>,
-    zDAORegistry: MockContract<ZDAORegistry>,
+  let zDAORegistry: MockContract<ZDAORegistry>,
     snapshotZDAOChef: MockContract<SnapshotZDAOChef>;
 
-  const zNAPairs: zNAPair[] = [];
+  const zDAOPairs: zDAOPair[] = [];
 
   const validateSnapshotDAOInformation = async (
     platformType: number,
     daoId: number,
     gnosisSafe: string,
     name: string,
-    ens: string,
-    zNAs: number[]
+    ens: string
   ) => {
     const zDAORecord = await zDAORegistry.zDAORecords(daoId);
     expect(zDAORecord.platformType.toNumber()).to.be.equal(platformType);
@@ -61,8 +57,6 @@ describe("ZDAORegistry", function () {
     const zDAORecord2 = await zDAORegistry.getZDAOById(daoId);
     expect(zDAORecord2.platformType.toNumber()).to.be.equal(platformType);
     expect(zDAORecord2.id.toNumber()).to.be.equal(daoId);
-    const zDAOZNAsAs = zDAORecord2.associatedzNAs.map((zNA) => zNA.toNumber());
-    zDAOZNAsAs.forEach((zNA, index) => expect(zNA).to.be.equal(zNAs[index]));
 
     const zDAOInfo = await snapshotZDAOChef.zDAOInfos(daoId);
     expect(zDAOInfo.id.toNumber()).to.be.equal(daoId);
@@ -73,14 +67,12 @@ describe("ZDAORegistry", function () {
   beforeEach("init setup", async function () {
     [deployer, user1, user2, user3] = await ethers.getSigners();
 
-    znsHub = (await smock.fake("IZNSHub")) as FakeContract<IZNSHub>;
-
     const zDAORegistryFactory = (await smock.mock<ZDAORegistry__factory>(
       "ZDAORegistry"
     )) as MockContractFactory<ZDAORegistry__factory>;
     zDAORegistry =
       (await zDAORegistryFactory.deploy()) as MockContract<ZDAORegistry>;
-    await zDAORegistry.__ZDAORegistry_init(znsHub.address);
+    await zDAORegistry.__ZDAORegistry_init();
 
     const snapshotZDAOChefFactory =
       (await smock.mock<SnapshotZDAOChef__factory>(
@@ -97,8 +89,7 @@ describe("ZDAORegistry", function () {
 
     const count = 3;
     for (let i = 0; i < count; i++) {
-      zNAPairs.push({
-        zNA: i + 1,
+      zDAOPairs.push({
         gnosisSafe: ethers.Wallet.createRandom().address,
         name: `ens${i + 1}-name`,
         ens: `ens${i + 1}`,
@@ -108,62 +99,51 @@ describe("ZDAORegistry", function () {
 
   describe("Check addNewDAO for snapshot", () => {
     it("adds new dao record", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(deployer.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(deployer.address);
-
       await zDAORegistry.addNewZDAO(
         PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
+        ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[0].ens])
       );
       await zDAORegistry.addNewZDAO(
         PlatformType.Snapshot, // platformType
-        zNAPairs[1].zNA, // zNA
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[1].ens])
+        zDAOPairs[1].gnosisSafe,
+        zDAOPairs[1].name,
+        ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[1].ens])
       );
 
       validateSnapshotDAOInformation(
         PlatformType.Snapshot,
         1,
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        zNAPairs[0].ens,
-        [1]
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
+        zDAOPairs[0].ens
       );
       validateSnapshotDAOInformation(
         PlatformType.Snapshot,
         2,
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].name,
-        zNAPairs[1].ens,
-        [2]
+        zDAOPairs[1].gnosisSafe,
+        zDAOPairs[1].name,
+        zDAOPairs[1].ens
       );
     });
 
     it("blocks when ens is already added", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(deployer.address);
-
       await zDAORegistry.addNewZDAO(
         PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
+        ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[0].ens])
       );
 
       await expect(
         zDAORegistry.addNewZDAO(
           PlatformType.Snapshot, // platformType
-          zNAPairs[0].zNA, // zNA
-          zNAPairs[0].gnosisSafe,
-          zNAPairs[0].name,
-          ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
+          zDAOPairs[0].gnosisSafe,
+          zDAOPairs[0].name,
+          ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[0].ens])
         )
-      ).to.revertedWith("Already added DAO with same zNA");
+      ).to.revertedWith("Already added zDAO with same name");
     });
   });
 
@@ -178,7 +158,6 @@ describe("ZDAORegistry", function () {
       platformType: number,
       daoId: number,
       gnosisSafe: string,
-      zNAs: number[],
       name: string,
       zDAOConfig: ZDAOConfig
     ) => {
@@ -191,11 +170,7 @@ describe("ZDAORegistry", function () {
       const zDAORecord2 = await zDAORegistry.getZDAOById(daoId);
       expect(zDAORecord2.platformType.toNumber()).to.be.equal(platformType);
       expect(zDAORecord2.id.toNumber()).to.be.equal(daoId);
-      const zDAOZNAsAs = zDAORecord2.associatedzNAs.map((zNA) =>
-        zNA.toNumber()
-      );
-      expect(zDAORecord.name).to.be.equal(zDAORecord2.name);
-      zDAOZNAsAs.forEach((zNA, index) => expect(zNA).to.be.equal(zNAs[index]));
+      expect(zDAORecord2.name).to.be.equal(zDAORecord.name);
 
       const zDAOAddr = await rootZDAOChef.zDAOs(daoId);
       const zDAO = await ethers.getContractAt(
@@ -254,7 +229,7 @@ describe("ZDAORegistry", function () {
         rootZDAOChef.address
       );
 
-      name = `${zNAPairs[0].zNA}.dao`;
+      name = `${zDAOPairs[0].ens}.dao`;
       zDAOConfig = {
         token: vToken.address,
         amount: BigNumber.from("10000").toNumber(),
@@ -268,9 +243,6 @@ describe("ZDAORegistry", function () {
     });
 
     it("adds new dao record", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(deployer.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(deployer.address);
-
       const options = ethers.utils.defaultAbiCoder.encode(
         [
           "address",
@@ -295,233 +267,42 @@ describe("ZDAORegistry", function () {
       );
       await zDAORegistry.addNewZDAO(
         PlatformType.Polygon, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
         options
       );
       await zDAORegistry.addNewZDAO(
         PlatformType.Polygon, // platformType
-        zNAPairs[1].zNA, // zNA
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].name,
+        zDAOPairs[1].gnosisSafe,
+        zDAOPairs[1].name,
         options
       );
 
       validatePolygonDAOInformation(
         PlatformType.Polygon,
         1,
-        zNAPairs[0].gnosisSafe,
-        [1],
-        zNAPairs[0].name,
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
         zDAOConfig
       );
       validatePolygonDAOInformation(
         PlatformType.Polygon,
         2,
-        zNAPairs[1].gnosisSafe,
-        [2],
-        zNAPairs[1].name,
+        zDAOPairs[1].gnosisSafe,
+        zDAOPairs[1].name,
         zDAOConfig
       );
-    });
-
-    it("blocks when same zNA was already added", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(deployer.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(deployer.address);
-
-      const options = ethers.utils.defaultAbiCoder.encode(
-        [
-          "address",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "bool",
-        ],
-        [
-          zDAOConfig.token,
-          zDAOConfig.amount,
-          zDAOConfig.duration,
-          zDAOConfig.votingDelay,
-          zDAOConfig.votingThreshold,
-          zDAOConfig.minimumVotingParticipants,
-          zDAOConfig.minimumTotalVotingTokens,
-          zDAOConfig.isRelativeMajority,
-        ]
-      );
-      await zDAORegistry.addNewZDAO(
-        PlatformType.Polygon, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        options
-      );
-      await expect(
-        zDAORegistry.addNewZDAO(
-          PlatformType.Polygon, // platformType
-          zNAPairs[0].zNA, // zNA
-          zNAPairs[0].gnosisSafe,
-          zNAPairs[1].name,
-          options
-        )
-      ).to.be.revertedWith("Already added DAO with same zNA");
-    });
-  });
-
-  describe("Check addZNAAssociation", () => {
-    it("adds new zna association", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(user2.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[2].zNA).returns(user3.address);
-
-      await zDAORegistry.connect(user1).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
-      );
-      await zDAORegistry.connect(user2).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[1].zNA, // zNA
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[1].ens])
-      );
-
-      await zDAORegistry.connect(user3).addZNAAssociation(1, zNAPairs[2].zNA);
-
-      expect(await zDAORegistry.zNATozDAOId(zNAPairs[0].zNA)).to.eq(
-        BigNumber.from(1)
-      );
-      expect(await zDAORegistry.zNATozDAOId(zNAPairs[1].zNA)).to.eq(
-        BigNumber.from(2)
-      );
-      expect(await zDAORegistry.zNATozDAOId(zNAPairs[2].zNA)).to.eq(
-        BigNumber.from(1)
-      );
-
-      validateSnapshotDAOInformation(
-        PlatformType.Snapshot,
-        1,
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].ens,
-        zNAPairs[0].name,
-        [zNAPairs[0].zNA, zNAPairs[2].zNA]
-      );
-      validateSnapshotDAOInformation(
-        PlatformType.Snapshot,
-        2,
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].ens,
-        zNAPairs[1].name,
-        [zNAPairs[1].zNA]
-      );
-    });
-
-    it("renews existing association", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(user2.address);
-
-      await zDAORegistry.connect(user1).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
-      );
-      await zDAORegistry.connect(user2).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[1].zNA, // zNA
-        zNAPairs[1].gnosisSafe,
-        zNAPairs[1].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[1].ens])
-      );
-
-      await zDAORegistry.connect(user2).addZNAAssociation(1, zNAPairs[1].zNA);
-      await expect(
-        zDAORegistry.connect(user2).addZNAAssociation(1, zNAPairs[1].zNA)
-      ).to.be.revertedWith("zNA already linked to DAO");
-
-      expect(await zDAORegistry.zNATozDAOId(zNAPairs[1].zNA)).to.eq(
-        BigNumber.from(1)
-      );
-
-      validateSnapshotDAOInformation(
-        PlatformType.Snapshot,
-        1,
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].ens,
-        zNAPairs[0].name,
-        [zNAPairs[0].zNA, zNAPairs[1].zNA]
-      );
-    });
-  });
-
-  describe("Check removezNAAssociation", () => {
-    it("removes existing association", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(user2.address);
-
-      await zDAORegistry.connect(user1).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
-      );
-      await zDAORegistry.connect(user2).addZNAAssociation(1, zNAPairs[1].zNA);
-      await zDAORegistry
-        .connect(user1)
-        .removeZNAAssociation(1, zNAPairs[0].zNA);
-
-      validateSnapshotDAOInformation(
-        PlatformType.Snapshot,
-        1,
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].ens,
-        zNAPairs[0].name,
-        [zNAPairs[1].zNA]
-      );
-      expect(
-        (await zDAORegistry.zNATozDAOId(zNAPairs[1].zNA)).toNumber()
-      ).to.eq(1);
-    });
-    it("reverts", async function () {
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-      znsHub.ownerOf.whenCalledWith(zNAPairs[1].zNA).returns(user2.address);
-
-      await zDAORegistry.connect(user1).addNewZDAO(
-        PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
-      );
-      await zDAORegistry
-        .connect(user1)
-        .removeZNAAssociation(1, zNAPairs[0].zNA);
-
-      await expect(
-        zDAORegistry.connect(user1).removeZNAAssociation(1, zNAPairs[0].zNA)
-      ).to.revertedWith("zNA not associated");
     });
   });
 
   describe("Check ResourceRegistry", () => {
     it("resource should exist", async function () {
       // Add new DAO
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-
       await zDAORegistry.connect(user1).addNewZDAO(
         PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
+        ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[0].ens])
       );
 
       const zDAOId = 1, emptyZDAOID = 0;
@@ -534,14 +315,11 @@ describe("ZDAORegistry", function () {
 
     it("destroyed resource should not exist", async function () {
       // Add new DAO
-      znsHub.ownerOf.whenCalledWith(zNAPairs[0].zNA).returns(user1.address);
-
       await zDAORegistry.connect(user1).addNewZDAO(
         PlatformType.Snapshot, // platformType
-        zNAPairs[0].zNA, // zNA
-        zNAPairs[0].gnosisSafe,
-        zNAPairs[0].name,
-        ethers.utils.defaultAbiCoder.encode(["string"], [zNAPairs[0].ens])
+        zDAOPairs[0].gnosisSafe,
+        zDAOPairs[0].name,
+        ethers.utils.defaultAbiCoder.encode(["string"], [zDAOPairs[0].ens])
       );
 
       // Destroy it
