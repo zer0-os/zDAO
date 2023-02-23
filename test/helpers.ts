d540dbca2220a410aeabdbcbf3843ecda6ab1316
deep-quality-dev <hungrywarrior081@gmail.com>
2023-02-23 11:24:29 -0500

    feat: add hardhat helper functions

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,5 +1,17 @@
 import { ethers } from "hardhat";
 
+export const increaseTime = async (seconds: number) => {
+  await ethers.provider.send("evm_increaseTime", [seconds]);
+  await ethers.provider.send("evm_mine", []);
+};
+
+// block time in second unit
+export const mineToBlock = async (blockNumber: number, blockTime = 13) => {
+  for (let i = 0; i < blockNumber; i++) {
+    await increaseTime(blockTime);
+  }
+};
+
 export const takeSnapshot = async () => {
   const result = await ethers.provider.send("evm_snapshot", []);
   await mineToBlock(1);
